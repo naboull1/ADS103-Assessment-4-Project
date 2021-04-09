3fb34a806de683e262d562ed69400df0a1e85af5
Luaay <naboulsi.luaay@gmail.com>
2021-04-10 04:31:53 +1000

    update

---
+++ ADS Assessment-4-Project/Main.cpp
@@ -0,0 +1,29 @@
+/// Luaay Naboulsi
+/// ADS103 Algorithmsand Data Structures
+/// Assessment 4 Question 1
+
+#include <iostream>
+#include <string>
+#include <windows.h>								//Library for controling windows functions
+#include <fstream>									//Library for File System control
+#include <chrono>									//Library for Time Control
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+//Main Master Code
+void main()
+{
+	//Declaration of Variables
+	int firstLineVariable;
+
+	//Reads in textfile and stores information into Variables
+	ifstream readFile;
+	readFile.open("input-q1a2.txt");
+	readFile >> firstLineVariable;
+
+
+
+}
